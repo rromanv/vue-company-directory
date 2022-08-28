1479e1d6146f0a82ffd5f957c0c4bfd2ab94e009
Rodolfo Roman <rromanv@gmail.com>
2022-08-28 03:31:43 -0500

    added cards

--- src/components/MainSearch.vue
+++ src/components/MainSearch.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="wrapper">
     <input type="text" placeholder="Search Staff Member" class="search" />
-    <span class="icon"
-      ><svg
+    <span class="icon">
+      <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
