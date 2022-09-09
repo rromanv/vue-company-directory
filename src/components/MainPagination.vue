2249d1214caa5d5d8fda5056b02be3dbafefe4ce
Rodolfo Roman <rromanv@gmail.com>
2022-09-09 17:16:16 -0500

    better practices applied, corrected lint rules

--- src/components/MainPagination.vue
+++ src/components/MainPagination.vue
@@ -14,11 +14,18 @@
     }
   }
 </script>
+s
 
 <template>
   <div class="pagination">
     <button class="action" :disabled="activePage === 1" @click="prevPage">Prev</button>
-    <button class="page" v-for="page in pages" :class="page === activePage ? 'active' : ''" @click="activePage = page">
+    <button
+      v-for="page in pages"
+      :key="page"
+      class="page"
+      :class="page === activePage ? 'active' : ''"
+      @click="activePage = page"
+    >
       {{ page }}
     </button>
     <button class="action" :disabled="activePage === pages" @click="nextPage">Next</button>
