6e080600bc81b5ff531b4bffc11fa083b055e68d
Rodolfo Roman <rromanv@gmail.com>
2022-11-19 00:41:10 -0600

    fix url?

--- src/composables/useAPI.js
+++ src/composables/useAPI.js
@@ -8,6 +8,9 @@ const loading = ref(false)
 const pageSize = ref(8)
 
 const api = axios.create({
+  baseURL: import.meta.env.PROD
+    ? import.meta.env.VITE_API_URL
+    : import.meta.env.BASE_URL,
   auth: {
     username: import.meta.env.VITE_API_USERNAME,
     password: import.meta.env.VITE_API_PASSWORD,
