b5b7a8bc505c9f25feecb2760ce44c122149fced
Rodolfo Roman <rromanv@gmail.com>
2022-11-19 20:38:47 -0600

    added employee data and department

--- src/components/MainCards.vue
+++ src/components/MainCards.vue
@@ -1,10 +1,18 @@
 <script setup>
+  import useAPI from '@/composables/useAPI'
   import MainCardsSingle from '@/components/MainCardsSingle.vue'
+
+  const { employees } = useAPI()
 </script>
 
 <template>
   <div class="sub-wrapper">
-    <MainCardsSingle v-for="n in 20" :key="n" />
+    <Suspense>
+      <MainCardsSingle v-for="employee in employees" :key="employee.employeeId" :employee="employee" />
+      <template #fallback>
+        <div>Loading...</div>
+      </template>
+    </Suspense>
   </div>
 </template>
 
--- src/components/MainCardsSingle.vue
+++ src/components/MainCardsSingle.vue
@@ -1,13 +1,35 @@
 <script setup>
+  import { ref } from 'vue'
   import { faker } from '@faker-js/faker'
 
-  const firstName = faker.name.firstName()
-  const lastName = faker.name.lastName()
-  const fullName = `${firstName} ${lastName}`
+  import useAPI from '@/composables/useAPI'
+  const { getDepartment } = useAPI()
 
   const selectCard = () => {
-    console.log(`${fullName} selected`)
+    console.log(`${props.employee.name} selected`)
   }
+
+  const props = defineProps({
+    employee: {
+      type: Object,
+      required: true,
+      default: () => {
+        return {
+          createdAt: '2022-01-01',
+          departmentId: '123',
+          email: 'john.doe@example.com',
+          employeeId: '123',
+          name: 'John Doe',
+          quote: 'Really Cool quote',
+          title: 'Position',
+          updatedAt: '2022-01-01',
+        }
+      },
+    },
+  })
+
+  const departmentResponse = await getDepartment(props.employee.departmentId)
+  const department = ref(departmentResponse)
 </script>
 
 <template>
@@ -16,9 +38,9 @@
       <img :src="faker.internet.avatar()" alt="" srcset="" />
     </div>
     <div class="card-details">
-      <p class="card-details-name">{{ fullName }}</p>
-      <p class="card-details-job">{{ faker.name.jobTitle() }}, {{ faker.name.jobArea() }}</p>
-      <p class="card-details-quote">"{{ faker.lorem.paragraph() }}"</p>
+      <p class="card-details-name">{{ props.employee.name }}</p>
+      <p class="card-details-job">{{ props.employee.title }}, {{ department.name }}</p>
+      <p class="card-details-quote">"{{ props.employee.quote }}"</p>
     </div>
   </div>
 </template>
